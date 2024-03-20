3505ed90801b40659dfc6ebbcc1dd8ade7d281ec
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-03-20 20:38:50 -0300

    raw: criado estrutura basica do banco de dados

--- backend/src/db/db-create.js
+++ backend/src/db/db-create.js
@@ -8,6 +8,61 @@ const sequelize = new Sequelize({
     password: 'admin'
 });
 
+const Produto = sequelize.define('produto', {
+    nome: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    codigo_barras: {
+        type: DataTypes.INTEGER(13),
+    },
+    valor_venda: {
+        type: DataTypes.DECIMAL(6,2),
+        default: 0
+    },
+    quantidade: {
+        type: DataTypes.DECIMAL(6,2),
+        default: 0
+    }
+}, { sequelize, paranoid: true });
+
+const Venda = sequelize.define('venda',{
+    valor_total: {
+        type: DataTypes.DECIMAL(6,2),
+        // allowNull: false verificar a necessidade
+    },
+    processada: {
+        type: DataTypes.SMALLINT(1),
+        default: 0
+    },
+    data_emissao:{
+        type: DataTypes.DATE
+    }
+
+}, { sequelize, paranoid: true});
+
+const Itens_venda = sequelize.define('itens_venda',{
+    quantidade:{
+        type: DataTypes.DECIMAL(6,2),
+        allowNull: false
+    },
+    valor_unitario:{
+        type: DataTypes.DECIMAL(6,2),
+        allowNull: false
+    },
+    valor_total_item:{
+        type: DataTypes.DECIMAL(6,2),
+    }
+}, { sequelize, paranoid: true});
+
+Produto.hasMany(Itens_venda,{foreignKey: 'produto_id'});
+Itens_venda.belongsTo(Produto, {foreignKey: 'produto_id'});
+Venda.hasMany(Itens_venda,{foreignKey:'venda_id'});
+Itens_venda.belongsTo(Venda,{foreignKey:'venda_id'});
+
+
 module.exports = {
     sequelize,
-}
+    Produto,
+    Venda
+};
