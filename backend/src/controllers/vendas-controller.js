a66ba1a41f42cca7e49fc2f218c3c291d08329bf
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-03-24 10:48:52 -0300

    feat: adicionado função de atualizar valor_total da venda

--- backend/src/controllers/vendas-controller.js
+++ backend/src/controllers/vendas-controller.js
@@ -23,6 +23,8 @@ const adicionar_item = async (req, res) => {
         const venda_id = req.params.venda_id
 
         const novo_item = await db.Itens_venda.create({ quantidade, valor_unitario, valor_total_item, produto_id, venda_id })
+        
+        atualizar_valor_venda(venda_id)
 
         res.json(novo_item)
 
@@ -55,6 +57,13 @@ const consultar_venda_especifica = async (req, res) => {
     } catch (error) {
         res.json(error.message)
     }
+}
+
+const atualizar_valor_venda = async (venda_id) => {
+    const valor_total = await db.Itens_venda.sum('valor_total_item', {where:{ venda_id: venda_id}})
+    const venda = await db.Venda.update({valor_total},{where:{ id: venda_id }})
+    console.log(venda)
+    console.log(`Valor da venda ${venda_id} atualizado para ${valor_total}`)
 }
 
 module.exports = {
