fd0782596cb73acbc47e651713af4ebb008d732e
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-03-26 20:18:34 -0300

    feat: criado componentes de lista-dados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarDefaultComponent } from './components/navbar-default/navbar-default.component';
+import { ListaDadosComponent } from './components/lista-dados/lista-dados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarDefaultComponent
+    NavbarDefaultComponent,
+    ListaDadosComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/lista-dados/lista-dados.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-dados',
+  templateUrl: './lista-dados.component.html',
+  styleUrl: './lista-dados.component.css'
+})
+export class ListaDadosComponent {
+  dados =  [
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    },
+    {
+      id:123,
+      nome:'produto de teste',
+      codigo_barras: 7894900027013,
+      quantidade:10,
+      preco_venda:10
+    }
+  ]
+}
