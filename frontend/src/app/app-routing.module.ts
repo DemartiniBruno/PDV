2ac427e056a597b0170a387ea6c27555dbdb6bca
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-04-01 16:59:16 -0300

    feat: adicionando rotas de cadastro de venda/produtos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { ProdutosComponent } from './pages/produtos/produtos.component';
 import { VendasComponent } from './pages/vendas/vendas.component';
 import { DetalheProdutoComponent } from './pages/detalhe-produto/detalhe-produto.component';
 import { DetalheVendaComponent } from './pages/detalhe-venda/detalhe-venda.component';
-// import { CadastroProdutoComponent } from './pages/cadastro-produto/cadastro-produto.component';
-// import { CadastroVendaComponent } from './pages/cadastro-venda/cadastro-venda.component';
+import { CadastroProdutoComponent } from './pages/cadastro-produto/cadastro-produto.component';
+import { CadastroVendaComponent } from './pages/cadastro-venda/cadastro-venda.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'produtos', component: ProdutosComponent},
   {path: 'produtos/:id', component: DetalheProdutoComponent},
+  {path: 'produtos/novo', component: CadastroProdutoComponent},
   {path: 'vendas', component: VendasComponent},
   {path: 'vendas/:id', component: DetalheVendaComponent},
+  {path: 'vendas/novo', component: CadastroVendaComponent},
 ];
 
 @NgModule({
