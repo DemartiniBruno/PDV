4f3bb0dd91982a07961666c52a0d76adadb47b01
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-04-23 22:54:59 -0300

    feat: adicionado tratamento de status na listagem das vendas

--- frontend/src/app/pages/vendas/vendas.component.ts
+++ frontend/src/app/pages/vendas/vendas.component.ts
@@ -15,8 +15,24 @@ export class VendasComponent {
   }
 
   getVendas(): void{
-    this.vendasService.getAll().subscribe(vendas => (this.vendas = vendas))
+    this.vendasService.getAll().subscribe(vendas => {
+      this.vendas = vendas
+      this.vendas.forEach(venda=>{
+        switch(venda.status){
+          case 0:
+            venda.status='Venda Pendente'
+            break
+          case 1:
+            venda.status='Venda Concluída'
+            break
+          case 2:
+            venda.status='Venda Cancelada'
+        }
+      })
+    })
+
   }
+
 
   colunas=['id' , 'numero_venda', 'status', 'valor_total']
   clickedRows = new Set<Vendas>()
